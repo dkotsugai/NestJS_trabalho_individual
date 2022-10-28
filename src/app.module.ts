6c3360bd3d33cc98e4fa65ff8cc71b085f259362
Daniel Kotsugai <d.kotsugai.sarmento@avanade.com>
2022-10-28 11:59:59 -0300

    trabalhoIndividual

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
 import { RegisterModule } from './register/register.module';
 
 @Module({
-  imports: [UserModule, RegisterModule],
+  imports: [RegisterModule],
   controllers: [AppController],
   providers: [AppService],
 })
